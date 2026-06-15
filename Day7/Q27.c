6844cadb65ac6df16387f31e14487e0ec14d3b38
Yash Thenua <yash.thenua2025@glbajajgroup.org>
2026-06-15 15:26:09 +0530

    Add Day7 programs

---
+++ Day7/Q27.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int sumOfDigits(int n) {
+    if (n == 0)
+    return 0;
+    return (n % 10) + sumOfDigits(n / 10);
+}
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("Sum of digits = %d", sumOfDigits(num));
+    return 0;
+}
