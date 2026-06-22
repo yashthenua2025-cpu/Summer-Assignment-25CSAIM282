36b8e77486283e5835c98300f1160c5b8028a3b3
Yash Thenua <yash.thenua2025@glbajajgroup.org>
2026-06-22 07:11:49 +0530

    Added Day 14 assignment files Q53-Q56

---
+++ Q55.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    int n, i;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int largest = arr[0];
+    int secondLargest = arr[0];
+    for(i = 1; i < n; i++) {
+        if(arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+        else if(arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+    printf("Second Largest Element = %d", secondLargest);
+    return 0;
+}
