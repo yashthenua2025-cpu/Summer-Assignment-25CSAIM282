0bf5a14105b41e198beae14ef7b4928dfa8f1b17
Yash Thenua <yash.thenua2025@glbajajgroup.org>
2026-06-30 00:13:45 +0530

    Added Day21 programs (Q81-Q84)

---
+++ Day21/Q81.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    while (str[i] != '\0') {
+        i++;
+    }
+
+    printf("Length of string = %d\n", i);
+
+    return 0;
+}
