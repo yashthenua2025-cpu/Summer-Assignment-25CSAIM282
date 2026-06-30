23c79f81384e6807e39fb09a2b085eeb6b2269d8
Yash Thenua <yash.thenua2025@glbajajgroup.org>
2026-06-30 16:43:18 +0530

    Added Day22 programs (Q85-Q88)

---
+++ Day22/Q86.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    char str[100];
+    int i = 0, words = 1;
+    printf("Enter a sentence: ");
+    gets(str);
+    while (str[i] != '\0') {
+        if (str[i] == ' ')
+            words++;
+        i++;
+    }
+    printf("Number of words = %d\n", words);
+    return 0;
+}
